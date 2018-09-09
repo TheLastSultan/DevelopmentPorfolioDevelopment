ed259b8f3507d4024033b513f7b153879163f345
Meezo <mozeiny@gmail.com>
2018-09-08 20:43:20 -0700

    remodeling the about me page

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,41 +36,59 @@ end
 
 puts "10 blog posts created"
 
-5.times do |skill|
-  Skill.create!(
-    title: "Rails #{skill}",
-    percent_utilized: 15
-  )
-end
+Skill.create!(
+  title: "SQL",
+  percent_utilized: 70
+)
+
+Skill.create!(
+  title: "Ruby & Rails",
+  percent_utilized: 90
+)
+
+Skill.create!(
+  title: "Python",
+  percent_utilized: 67
+)
+
+Skill.create!(
+  title: "CSS&HTML",
+  percent_utilized: 35
+)
+
+Skill.create!(
+  title: "React",
+  percent_utilized: 66
+)
 
 puts "5 skills created"
 
-8.times do |portfolio_item|
-  Portfolio.create!(
-    title: "Portfolio title: #{portfolio_item}",
-    subtitle: "Ruby on Rails",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200"
-  )
-end
+# 8.times do |portfolio_item|
+#   Portfolio.create!(
+#     title: "Portfolio title: #{portfolio_item}",
+#     subtitle: "Ruby on Rails",
+#     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+#     main_image: "http://placehold.it/600x400",
+#     thumb_image: "http://placehold.it/350x200"
+#   )
+# end
 
-1.times do |portfolio_item|
-  Portfolio.create!(
-    title: "Portfolio title: #{portfolio_item}",
-    subtitle: "Angular",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200"
-  )
-end
+# 1.times do |portfolio_item|
+#   Portfolio.create!(
+#     title: "Portfolio title: #{portfolio_item}",
+#     subtitle: "Angular",
+#     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+#     main_image: "http://placehold.it/600x400",
+#     thumb_image: "http://placehold.it/350x200"
+#   )
+# end
 
 puts "9 portfolio items created"
 
-3.times do |technology|
-  Portfolio.last.technologies.create!(
-    name: "Technology #{technology}"
-  )
-end
+# 3.times do |technology|
+#   Portfolio.last.technologies.create!(
+#     name: "Technology #{technology}"
+#   )
+# end
 
-puts "3 technologies created"
+# puts "3 technologies created"
