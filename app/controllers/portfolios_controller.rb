f6548b5baceb910f8c6d628cd547d0318be433e1
Meezo <mozeiny@gmail.com>
2018-11-10 19:45:59 -0800

    added filtering on the showpage

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -17,8 +17,9 @@ class PortfoliosController < ApplicationController
   end
 
   def search
-    byebug
-    @portfolio_items = Portfolio.joins(:technologies).where('name LIKE ?', 'React')
+    @technologies = Technology.all.uniq
+    @portfolio_items = Portfolio.joins(:technologies).where('name LIKE ?', 'React').by_position
+
     render :index
   end 
 
@@ -82,5 +83,6 @@ class PortfoliosController < ApplicationController
 
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
+    @technologies = Technology.all.uniq
   end
 end
